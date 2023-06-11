2c62c6ccb7d145b789c0c822eae7af63d8a6c862
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2023-06-11 15:54:33 +0330

    chnage

--- R/positiveStein.R
+++ R/positiveStein.R
@@ -1,13 +1,28 @@
-positiveStein <- function(X,y, H, h, q){
-    n <- dim(X)[1]
-    p <- dim(X)[2]
-    m <- n - p
-    d <- ((q-2)*m)/(q*(m+2))
-    u_est <- unrestricted(X, y)
-    r_est <- restricted(X, y, H, h)
-    test_stat <- test_statistics(X, y, H, h, q)
-    threshold <- qf(1-alpha,q, n-p)
-    r_est + (1 - d/test_stat)* int(test_stat > d) * (u_est-r_est)
+#' positiveStein
+#'
+#' positiveStein
+#'
+#' @param x scaler.
+#' @param q scaler.
+#' @param n number.
+#' @param alpha scaler.
+#' @param G scaler.
+#'
+#' @references ref
+#' @examples
+#' print(x)
+#' @export
+
+positiveStein <- function(X, y, H, h, q) {
+  n <- dim(X)[1]
+  p <- dim(X)[2]
+  m <- n - p
+  d <- ((q - 2) * m) / (q * (m + 2))
+  u_est <- unrestricted(X, y)
+  r_est <- restricted(X, y, H, h)
+  test_stat <- test_statistics(X, y, H, h, q)
+  threshold <- qf(1 - alpha, q, n - p)
+  r_est + (1 - d / test_stat) * int(test_stat > d) * (u_est - r_est)
 }
 
 
