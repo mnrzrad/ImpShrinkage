b0bd3cc71a5129a8475bd4c67a0e5059ee2bd9a9
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2023-06-12 16:20:16 +0330

    change example

--- R/simdata.R
+++ R/simdata.R
@@ -30,20 +30,22 @@
 #'  Saleh, A. K. Md. Ehsanes. (2006). \emph{Theory of Preliminary Test and Stein‐Type Estimation With Applications}, Wiley.
 #'
 #' @examples
-#' simulated_data <- simulate(n = 100, p = 5)
+#' simulated_data <- simdata(n = 100, p = 5)
 #' X <- simulated_data$X
 #' y <- simulated_data$y
-#'
-#' simulated_data <- simulate(n = 100, p = 5, beta = c(2, 1, 3, 0, 5))
+#' X
+#' y
+#' simulated_data <- simdata(n = 100, p = 5, beta = c(2, 1, 3, 0, 5))
 #' X <- simulated_data$X
 #' y <- simulated_data$y
-#'
+#' X
+#' y
 #' @export
 
 simdata <- function(n, p, beta = rep(1, p), seed = NULL) {
   set.seed(seed)
 
-  X <- matrix(rnorm(n * p), nr = n, nc = p)
+  X <- matrix(rnorm(n * p), nrow = n, ncol = p)
 
   y <- X %*% beta + rnorm(n)
 
