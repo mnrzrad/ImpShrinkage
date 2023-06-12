d3661801a60976da4ae565101c1d475cfc7e5ca8
mnrzrad <mina.norouzirad@gmail.com>
2023-06-12 14:08:51 +0100

    update by removing default from simulation function

--- R/simdata.R
+++ R/simdata.R
@@ -3,22 +3,13 @@
 #' This function generates simulated data from a standard normal distribution as an example.
 #'
 #' @param n Number of observations.
-#'   An integer value specifying the total number of observations in the dataset.
-#'   The function expects a positive integer greater than zero.
 #'
 #' @param p Number of variables.
-#'   An integer value indicating the number of predictor variables in the regression model.
-#'   The function expects a positive integer greater than zero and more than \code{n}.
 #'
 #' @param beta Regression parameter.
-#'   A numeric vector of length p representing the regression coefficients for the predictor variables.
-#'   Each element in the vector corresponds to the coefficient for a specific predictor variable.
-#'   The function uses these coefficients to estimate the relationship between the predictor variables and the response variable.
-#'   The length of the vector must be equal to the value specified in the \code{p} parameter.
-#'   If not provided, default values may be used.
 #'
-#' @param seed (Optional) The random seed for reproducibility.
-#'   An integer value representing the seed for the random number generator. Default is NULL.
+#'
+#' @param seed (Optional) The random seed for reproducibility. Default is NULL.
 #'
 #' @return A list containing the following components:
 #' \describe{
@@ -42,7 +33,7 @@
 #' y
 #' @export
 
-simdata <- function(n, p, beta = rep(1, p), seed = NULL) {
+simdata <- function(n, p, beta, seed = NULL) {
   set.seed(seed)
 
   X <- matrix(rnorm(n * p), nrow = n, ncol = p)
