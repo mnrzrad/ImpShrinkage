750fff430626099ff47d8e304e91b4985caa55e5
ricardomourarpm <ricardo.moura.rpm@gmail.com>
2023-06-12 16:17:42 +0100

    Update Stein.R

--- R/Stein.R
+++ R/Stein.R
@@ -1,31 +1,31 @@
 #' The Stein estimator
 #'
-#' This function calculates the Stein estimator
+#' This function can be used to calculate the Stein estimator
 #'
-#' @param X Input matrix, of dimension nobs x nvars; each row is an observation vector.
-#' @param y Quantitative response variable.
-#' @param H A given q x p matrix.
-#' @param h A given q x 1 vector.
+#' @param X Matrix with input observations, of dimension n_obs x p_vars; each row is an observation vector.
+#' @param y Univariate quantitative response variable with dimension n_obs
+#' @param H A given q_restr x p_vars matrix.
+#' @param h A given q_restr x 1 vector.
 #'
-#' @return A vector of coefficients
+#' @return A vector of regression coefficients
 #'
 #' @references
 #'  Saleh, A. K. Md. Ehsanes. (2006). \emph{Theory of Preliminary Test and Stein‐Type Estimation With Applications}, Wiley.
 #'
 #' @examples
-#' n <- 100
-#' p <- 5
+#' n_obs <- 100
+#' p_vars <- 5
 #' beta <- c(2, 1, 3, 0, 5)
-#' simulated_data <- simdata(n, p, beta)
+#' simulated_data <- simdata(n = n_obs,  p = p_vars, beta)
 #' X <- simulated_data$X
 #' y <- simulated_data$y
 #' # H beta = h
-#' H <- matrix(c(1,1,-1,0,0,1,0,1,0,-1,0,0,0,1,0), nr = 3, nc = p, byrow = TRUE)
+#' H <- matrix(c(1,1,-1,0,0,1,0,1,0,-1,0,0,0,1,0), nrow = 3, ncol = p, byrow = TRUE)
 #' h <- rep(0, nrow(H))
 #' Stein(X, y, H, h)
 #'
 #' # H beta != h
-#' H <- matrix(c(1,1,-1,0,0,1,0,1,0,-1,0,0,0,1,0), nr = 3, nc = p, byrow = TRUE)
+#' H <- matrix(c(1,1,-1,0,0,1,0,1,0,-1,0,0,0,1,0), nrow = 3, ncol = p, byrow = TRUE)
 #' h <- rep(1, nrow(H))
 #' Stein(X, y, H, h)
 #' @export
